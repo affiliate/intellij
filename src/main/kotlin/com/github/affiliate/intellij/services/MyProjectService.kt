b3e91198ab25568b6c2ba34a5835d81698135cab
GitHub Action <action@github.com>
2022-09-19 07:27:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/affiliate/intellij/services/MyProjectService.kt
+++ src/main/kotlin/com/github/affiliate/intellij/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.affiliate.intellij.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.affiliate.intellij.MyBundle
 
 class MyProjectService(project: Project) {
 
